602b608fff61133211e05be6c1910bcef4f79d88
Faruk Hossain <miloniitju@gmail.com>
2025-03-04 21:49:35 +0800

    Initial Code

---
+++ src/main/java/com/practice/gameDevelopment/GameDevelopmentApplication.java
@@ -0,0 +1,13 @@
+package com.practice.gameDevelopment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameDevelopmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameDevelopmentApplication.class, args);
+	}
+
+}
