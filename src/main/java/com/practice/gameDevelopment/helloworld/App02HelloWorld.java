c7b5161caa01febb3f68f3c521f6f01e3e834c91
Faruk Hossain <miloniitju@gmail.com>
2025-03-05 23:39:39 +0800

    Add Beans with game

---
+++ src/main/java/com/practice/gameDevelopment/helloworld/App02HelloWorld.java
@@ -0,0 +1,34 @@
+package com.practice.gameDevelopment.helloworld;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.Arrays;
+
+public class App02HelloWorld {
+
+	public static void main(String[] args) {
+		// 1. Launch a Spring context
+		try (var context = new AnnotationConfigApplicationContext(HelloWorldConfiguration.class)) {
+			// 2. Configure the thing that we want spring to manage -
+			// HelloWorldConfiguration - @Configuration
+			// name - @Bean
+			System.out.println(context.getBean("name"));
+			System.out.println(context.getBean("age"));
+			System.out.println(context.getBean("person"));
+			System.out.println(context.getBean("personWithMethod"));
+			System.out.println(context.getBean("personWithParameters"));
+			System.out.println(context.getBean("person3WithParameters"));
+			System.out.println(context.getBean("person4WithQualifier"));
+			System.out.println(context.getBean(Person.class));
+			System.out.println(context.getBean("address2"));
+			System.out.println(context.getBean("address3"));
+			System.out.println(context.getBean(Address.class));
+
+			// prints all beans
+			Arrays.stream(context.getBeanDefinitionNames()).forEach(
+					System.out::println
+			);
+		}
+	}
+
+}
--- src/main/java/com/practice/gameDevelopment/helloworld/HelloWorldConfiguration.java
+++ src/main/java/com/practice/gameDevelopment/helloworld/HelloWorldConfiguration.java
@@ -1,4 +1,4 @@
-package com.practice.gameDevelopment;
+package com.practice.gameDevelopment.helloworld;
 
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
